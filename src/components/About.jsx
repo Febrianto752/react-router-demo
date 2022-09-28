11fc1f4283006d5f3d4786906b7d2b7486bc95d4
Febrianto <febrianto.bekasi@gmail.com>
2022-09-29 05:21:01 +0700

    learn create simple routing

---
+++ src/components/About.jsx
@@ -0,0 +1,10 @@
+const About = () => {
+  return (
+    <>
+      <h1>Halaman About</h1>
+      <a href="/">Back to home page</a>
+    </>
+  );
+};
+
+export default About;
---
+++ src/components/Home.jsx
@@ -0,0 +1,10 @@
+const Home = () => {
+  return (
+    <>
+      <h1>Halaman Home</h1>
+      <a href="/about">Go to about page</a>
+    </>
+  );
+};
+
+export default Home;
